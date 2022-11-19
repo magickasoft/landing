c3917e83753c35c83477b7bb818d10dad9ec8934
Evgeny Shmakov <sjs-master@yandex.ru>
2022-11-20 01:29:56 +0700

    fix: cmd5

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://localhost:3000',
+  siteUrl: 'https://madjoylab.ru',
   changefreq: 'daily',
   priority: 0.7,
   sitemapSize: 5000,
