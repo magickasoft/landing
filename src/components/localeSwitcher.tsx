971ccfb2ca4bbc0e8ad5001f216ed5269068db15
Evgeny Shmakov <sjs-master@yandex.ru>
2022-11-27 04:14:38 +0700

    fix: lint

--- src/components/localeSwitcher.tsx
+++ src/components/localeSwitcher.tsx
@@ -1,6 +1,6 @@
 import SC from '@emotion/styled';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 import React from 'react';
 
 const Container = SC.div`
@@ -12,22 +12,17 @@ const Locale = SC.span`
 `;
 
 export const LocaleSwitcher = () => {
-  const router = useRouter()
-  const { locales, pathname, query, asPath } = router;
+  const router = useRouter();
+  const {locales, pathname, query, asPath} = router;
   return (
     <Container>
-        {(locales || []).map((locale) => (
-            <Locale key={locale}>
-              <Link
-              href={{ pathname, query }}
-              as={asPath}
-              locale={locale}
-              legacyBehavior
-              >
-                {locale}
-              </Link>
-            </Locale>
-          ))}
+      {(locales || []).map(locale => (
+        <Locale key={locale}>
+          <Link href={{pathname, query}} as={asPath} locale={locale} legacyBehavior>
+            {locale}
+          </Link>
+        </Locale>
+      ))}
     </Container>
   );
 };
