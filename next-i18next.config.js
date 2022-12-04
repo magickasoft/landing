9bf405e820b6bbd39ff04177b9f362d1277895a9
Evgeny Shmakov <sjs-master@yandex.ru>
2022-12-04 17:17:29 +0700

    fix: localePath

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -10,11 +10,6 @@
         defaultLocale: 'ru',
         locales: ['ru', 'en'],
     },
-    /** To avoid issues when deploying to some paas (vercel...) */
-    localePath:
-      typeof window === 'undefined'
-        ? require('path').resolve('./public/locales')
-        : '/locales',
   
     reloadOnPrerender: process.env.NODE_ENV === 'development',
   
